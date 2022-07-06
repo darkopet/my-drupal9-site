ef346739e172dd15ee42180523c9fcce29b13df8
darkopet <darkodepetrovski@gmail.com>
2022-07-06 10:14:29 +0200

    files-styles

---
+++ web/sites/default/files/php/twig/62c43dd2bb0a9_datetime-wrapper.html.twi_zSPBLq3KkRxGi5DvXGWnetvOO/QnB7CPjZDSXxuedQHfRHGTWH51p0Nk6fA--zabTiJvs.php
@@ -0,0 +1,142 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* core/themes/claro/templates/datetime-wrapper.html.twig */
+class __TwigTemplate_187b7fa1ab959234fdd483b6b03cc644adf2a1be94ad1e6e31f47dd25d8de970 extends \Twig\Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $this->checkSecurity();
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        $__internal_b8a44bb7188f10fa054f3681425c559c29de95cd0490f5c67a67412aafc0f453 = $this->extensions["Drupal\\webprofiler\\Twig\\Extension\\ProfilerExtension"];
+        $__internal_b8a44bb7188f10fa054f3681425c559c29de95cd0490f5c67a67412aafc0f453->enter($__internal_b8a44bb7188f10fa054f3681425c559c29de95cd0490f5c67a67412aafc0f453_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "template", "core/themes/claro/templates/datetime-wrapper.html.twig"));
+
+        // line 12
+        $context["title_classes"] = [0 => "form-item__label", 1 => ((        // line 14
+($context["required"] ?? null)) ? ("js-form-required") : ("")), 2 => ((        // line 15
+($context["required"] ?? null)) ? ("form-required") : ("")), 3 => ((        // line 16
+($context["errors"] ?? null)) ? ("has-error") : (""))];
+        // line 19
+        echo "<div class=\"form-item form-datetime-wrapper\">
+";
+        // line 20
+        if (($context["title"] ?? null)) {
+            // line 21
+            echo "  <h4";
+            echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, ($context["title_attributes"] ?? null), "addClass", [0 => ($context["title_classes"] ?? null)], "method", false, false, true, 21), 21, $this->source), "html", null, true);
+            echo ">";
+            echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["title"] ?? null), 21, $this->source), "html", null, true);
+            echo "</h4>
+";
+        }
+        // line 23
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["content"] ?? null), 23, $this->source), "html", null, true);
+        echo "
+";
+        // line 24
+        if (($context["errors"] ?? null)) {
+            // line 25
+            echo "  <div class=\"form-item__error-message\">
+    ";
+            // line 26
+            echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["errors"] ?? null), 26, $this->source), "html", null, true);
+            echo "
+  </div>
+";
+        }
+        // line 29
+        if (($context["description"] ?? null)) {
+            // line 30
+            echo "  <div";
+            echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, ($context["description_attributes"] ?? null), "addClass", [0 => "form-item__description"], "method", false, false, true, 30), 30, $this->source), "html", null, true);
+            echo ">
+    ";
+            // line 31
+            echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["description"] ?? null), 31, $this->source), "html", null, true);
+            echo "
+  </div>
+";
+        }
+        // line 34
+        echo "</div>
+";
+        
+        $__internal_b8a44bb7188f10fa054f3681425c559c29de95cd0490f5c67a67412aafc0f453->leave($__internal_b8a44bb7188f10fa054f3681425c559c29de95cd0490f5c67a67412aafc0f453_prof);
+
+    }
+
+    public function getTemplateName()
+    {
+        return "core/themes/claro/templates/datetime-wrapper.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  88 => 34,  82 => 31,  77 => 30,  75 => 29,  69 => 26,  66 => 25,  64 => 24,  60 => 23,  52 => 21,  50 => 20,  47 => 19,  45 => 16,  44 => 15,  43 => 14,  42 => 12,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "core/themes/claro/templates/datetime-wrapper.html.twig", "/var/www/html/web/core/themes/claro/templates/datetime-wrapper.html.twig");
+    }
+    
+    public function checkSecurity()
+    {
+        static $tags = array("set" => 12, "if" => 20);
+        static $filters = array("escape" => 21);
+        static $functions = array();
+
+        try {
+            $this->sandbox->checkSecurity(
+                ['set', 'if'],
+                ['escape'],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->source);
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+}
