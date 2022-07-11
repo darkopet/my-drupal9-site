6b20e4446b747261394bf0886560369b2f46fe3e
darkopet <darkodepetrovski@gmail.com>
2022-07-11 12:46:42 +0200

    restart_settings

---
+++ web/sites/default/files/php/twig/62c6d00a16e74_field-group-html-element._8q3n50CaHU3U6upTPZZKYslGw/0Jg3QdWxzTzwIvyNsCb3z9AwrKmsWw9jgChsrQWaVwM.php
@@ -0,0 +1,138 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* modules/contrib/field_group/templates/field-group-html-element.html.twig */
+class __TwigTemplate_4e8f03367f2668f0257c8e2d3d7d81cc7ebb346aa069487f5a115b19bf368a66 extends \Twig\Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $this->checkSecurity();
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        $__internal_b8a44bb7188f10fa054f3681425c559c29de95cd0490f5c67a67412aafc0f453 = $this->extensions["Drupal\\webprofiler\\Twig\\Extension\\ProfilerExtension"];
+        $__internal_b8a44bb7188f10fa054f3681425c559c29de95cd0490f5c67a67412aafc0f453->enter($__internal_b8a44bb7188f10fa054f3681425c559c29de95cd0490f5c67a67412aafc0f453_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "template", "modules/contrib/field_group/templates/field-group-html-element.html.twig"));
+
+        // line 18
+        echo "
+<";
+        // line 19
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["wrapper_element"] ?? null), 19, $this->source), "html", null, true);
+        echo " ";
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["attributes"] ?? null), 19, $this->source), "html", null, true);
+        echo ">
+  ";
+        // line 20
+        if (($context["title"] ?? null)) {
+            // line 21
+            echo "  <";
+            echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["title_element"] ?? null), 21, $this->source), "html", null, true);
+            echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["title_attributes"] ?? null), 21, $this->source), "html", null, true);
+            echo ">";
+            echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["title"] ?? null), 21, $this->source), "html", null, true);
+            echo "</";
+            echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["title_element"] ?? null), 21, $this->source), "html", null, true);
+            echo ">
+  ";
+        }
+        // line 23
+        echo "  ";
+        if (($context["collapsible"] ?? null)) {
+            // line 24
+            echo "  <div class=\"field-group-wrapper\">
+  ";
+        }
+        // line 26
+        echo "  ";
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["children"] ?? null), 26, $this->source), "html", null, true);
+        echo "
+  ";
+        // line 27
+        if (($context["collapsible"] ?? null)) {
+            // line 28
+            echo "  </div>
+  ";
+        }
+        // line 30
+        echo "</";
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["wrapper_element"] ?? null), 30, $this->source), "html", null, true);
+        echo ">
+";
+        
+        $__internal_b8a44bb7188f10fa054f3681425c559c29de95cd0490f5c67a67412aafc0f453->leave($__internal_b8a44bb7188f10fa054f3681425c559c29de95cd0490f5c67a67412aafc0f453_prof);
+
+    }
+
+    public function getTemplateName()
+    {
+        return "modules/contrib/field_group/templates/field-group-html-element.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  82 => 30,  78 => 28,  76 => 27,  71 => 26,  67 => 24,  64 => 23,  53 => 21,  51 => 20,  45 => 19,  42 => 18,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "modules/contrib/field_group/templates/field-group-html-element.html.twig", "/var/www/html/web/modules/contrib/field_group/templates/field-group-html-element.html.twig");
+    }
+    
+    public function checkSecurity()
+    {
+        static $tags = array("if" => 20);
+        static $filters = array("escape" => 19);
+        static $functions = array();
+
+        try {
+            $this->sandbox->checkSecurity(
+                ['if'],
+                ['escape'],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->source);
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+}
