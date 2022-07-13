a3f1970e56aa91001ded972a4a376e6277ebdae1
darkopet <darkodepetrovski@gmail.com>
2022-07-13 12:40:00 +0200

    Remove all contrib themes, update composer.lock file.

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -778,3 +778,4 @@ if (file_exists(__DIR__ . '/settings.ddev.php') && getenv('IS_DDEV_PROJECT') == 
 # if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
 #   include $app_root . '/' . $site_path . '/settings.local.php';
 # }
+$config['system.logging']['error_level'] = 'verbose';
