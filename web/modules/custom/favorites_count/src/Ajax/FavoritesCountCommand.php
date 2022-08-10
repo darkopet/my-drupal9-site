2db2c99fe411504df8f5b04a9e84c569cc394542
darkopet <darkodepetrovski@gmail.com>
2022-08-10 15:46:20 +0200

    Favorites count block (+ Cache API). AJAX Callback skeleton.

--- web/modules/custom/favorites_count/src/Ajax/FavoritesCountCommand.php
+++ web/modules/custom/favorites_count/src/Ajax/FavoritesCountCommand.php
@@ -2,7 +2,29 @@
 
 namespace Drupal\favorites_count\Ajax;
 
-class FavoritesCountCommand
-{
+use Drupal\Core\Ajax\CommandInterface;
 
+/**
+ * Class ExampleCommand.
+ */
+class FavoritesCountCommandCommand implements CommandInterface {
+
+  protected $message;
+
+  public function __construct($message) {
+    $this->message = $message;
+  }
+
+  /**
+   * Render custom ajax command.
+   *
+   * @return ajax
+   *   Command function.
+   */
+  public function render() {
+    return [
+      'command' => 'favorites_count',
+      'message' => $this->message,
+    ];
+  }
 }
--- web/modules/custom/favorites_count/src/Plugin/Block/FavoritesCountBlock.php
+++ web/modules/custom/favorites_count/src/Plugin/Block/FavoritesCountBlock.php
@@ -4,9 +4,34 @@ namespace Drupal\favorites_count\Plugin\Block;
 
 use Drupal\Core\Block\BlockBase;
 use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
-
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\modules\contrib\FlagCountManager;
 
 class FavoritesCountBlock extends BlockBase implements ContainerFactoryPluginInterface {
+  /**
+   * @var $flagCounter getUserFlagFlaggingCount
+   */
+  protected $flagCounter;
+
+  /**
+   * @param ContainerInterface $container
+   * @param array $configuration
+   * @param string $plugin_id
+   * @param mixed $plugin_definition
+   *
+   * @return static
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('flag_counter'),
+    // $flag_service = Drupal::service('flag.count');
+    // $counts = $flag_service->getUserFlagFlaggingCount($node);
+    // if (settings.url.includes('/flag/flag') || settings.url.includes('flag/unflag'))
+    );
+  }
   /**
    * @param array $configuration
    * @param string $plugin_id
@@ -17,8 +42,9 @@ class FavoritesCountBlock extends BlockBase implements ContainerFactoryPluginInt
   {
     parent::__construct($configuration, $plugin_id, $plugin_definition);
     $this->flagCounter = $flagCounter;
+
   }
-  //if (settings.url.includes('/flag/flag') || settings.url.includes('flag/unflag'))
+
   /**
    * {@inheritdoc}
    */
