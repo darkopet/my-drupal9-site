8053b3fc0eb22f207deff02f3de0f7f093ef62de
darkopet <darkodepetrovski@gmail.com>
2022-08-15 11:23:04 +0200

    Custom field formatter. Up to default image from taxonomy term and image styles.

--- web/modules/custom/image_title_caption/src/Plugin/Field/FieldFormatter/ImageTitleCaption.php
+++ web/modules/custom/image_title_caption/src/Plugin/Field/FieldFormatter/ImageTitleCaption.php
@@ -2,16 +2,13 @@
 
 namespace Drupal\image_title_caption\Plugin\Field\FieldFormatter;
 
-use Drupal\Core\Entity\EntityDisplayRepositoryInterface;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Field\FieldDefinitionInterface;
 use Drupal\Core\Field\FieldItemListInterface;
-use Drupal\Core\Field\FormatterBase;
 use Drupal\Core\Field\Plugin\Field\FieldFormatter\EntityReferenceFormatterBase;
 use Drupal\Core\Logger\LoggerChannelFactoryInterface;
-use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 use Drupal\Core\Routing\CurrentRouteMatch;
-use Drupal\image\Plugin\Field\FieldFormatter\ImageFormatter;
+use Drupal\file\Entity\File;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
@@ -45,8 +42,6 @@ class ImageTitleCaption extends EntityReferenceFormatterBase {
    * @var \Drupal\Core\Entity\EntityTypeManagerInterface
    */
   protected $entityTypeManager;
-
-
 
   /**
    * Constructs an EntityReferenceEntityFormatter instance.
@@ -102,21 +97,36 @@ class ImageTitleCaption extends EntityReferenceFormatterBase {
    * {@inheritdoc}
    */
   public function viewElements(FieldItemListInterface $items, $langcode) {
+    $equipment = [];
     $node = $this->currentRouteService->getCurrentRouteMatch()->getParameter('node');
     if($node->get('type')->getValue()[0]['target_id'] === 'location') {
       $field_location_equipment = $node->get('field_location_equipment')->getValue();
-//      dd($field_location_equipment);
-
+      // Loop troug all equipment terms assigned to the field.
       foreach ($field_location_equipment as $taxonomyTermm){
+        // Get the term by term id.
         $term = \Drupal::entityTypeManager()->getStorage('taxonomy_term')->load($taxonomyTermm['target_id']);
-//        dd($term);
+          // Get the image file id from the term if there is one.
+          $imgTargetId = $term->get('field_image')->getValue()[0]['target_id'];
+          // Load the fiel by the id.
+          $file = File::load($imgTargetId);
+          if (!is_null($file)){
+            // Create the array with values for that equipment.
+            $equipment[] = [
+              'title' => $term->get('name')->getValue()[0]['value'],
+              'url' => $file->createFileUrl('TRUE')
+            ];
+          }
+          else {
+            // Find the default image from taxonomy term. NEED TO DO !
+            $equipment[] = [
+              'title' => $term->get('name')->getValue()[0]['value'],
+              'url' => 'test'
+            ];
+         }
       }
-//      dd($field_location_equipment);
     }
-//    dd($node);
       $element['#theme'] = 'image_title_caption';
-      $element['#title'] = $node->get('field_location_image')->getValue()[0]['title'] ;
-      $element['#url'] = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQs8EmDWAi3je4ZKxWbY7Tx5x6Et0JMEGRgXg&usqp=CAU';
+      $element['#images'] = $equipment;
 
     return $element;
   }
