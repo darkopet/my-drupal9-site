96bba2306e547e031dcd0abf03834770a3644e34
darkopet <darkodepetrovski@gmail.com>
2022-09-14 13:37:55 +0200

    Custom drush command.

---
+++ web/modules/custom/custom_drush_command/src/Commands/CustomDrushCommandCommands.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Drupal\custom_drush_command\Commands;
+
+use Consolidation\OutputFormatters\StructuredData\RowsOfFields;
+use Drush\Commands\DrushCommands;
+
+/**
+ * A Drush commandfile.
+ *
+ * In addition to this file, you need a drush.services.yml
+ * in root of your module, and a composer.json file that provides the name
+ * of the services file to use.
+ *
+ * See these files for an example of injecting Drupal services:
+ *   - http://cgit.drupalcode.org/devel/tree/src/Commands/DevelCommands.php
+ *   - http://cgit.drupalcode.org/devel/tree/drush.services.yml
+ */
+class CustomDrushCommandCommands extends DrushCommands {
+   /**
+   * Echos back hello with the argument provided.
+   *
+   * @param string $name
+   *   Argument provided to the drush command.
+   *
+   * @command drush9_example:hello
+   * @aliases d9-hello
+   * @options arr An option that takes multiple values.
+   * @options msg Whether or not an extra message should be displayed to the user.
+   * @usage drush9_example:hello akanksha --msg
+   *   Display 'Hello Akanksha!' and a message.
+   */
+  public function hello($name, $options = ['msg' => FALSE]) {
+    if ($options['msg']) {
+      $this->output()->writeln('Hello ' . $name . '! This is your first Drush 9 command.');
+    }
+    else {
+      $this->output()->writeln('Hello ' . $name . '!');
+    }
+  }
+
+}
