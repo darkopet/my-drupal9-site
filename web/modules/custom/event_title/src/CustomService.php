65ca39c37a003a1aa8e9051630809b3581c63c13
darkopet <darkodepetrovski@gmail.com>
2022-10-03 16:24:42 +0200

    Event adjustment on save. almmost done

--- web/modules/custom/event_title/src/CustomService.php
+++ web/modules/custom/event_title/src/CustomService.php
@@ -2,7 +2,7 @@
 
 namespace Drupal\event_title;
 
-use Drupal\node\NodeInterface;
+use Drupal\Core\Controller\TitleResolver;
 
 /**
  * Class CustomService
@@ -11,25 +11,36 @@ use Drupal\node\NodeInterface;
 
 class CustomService {
 
-  protected $eventTitle;
+  protected TitleResolver $getTitle;
 
   /**
    * CustomService constructor.
-   * @param NodeInterface $eventTitle
+   * @param TitleResolver $getTitle
    */
-  public function __construct(NodeInterface $eventTitle)
-  {
-    $this->eventTitle->getValue();
+  public function __construct(TitleResolver $getTitle) {
+//    dd($getTitle);
+    $this->getTitle = $getTitle;
   }
 
-  /**
-   * @return Drupal\Component\Render\MarkupInterface|string
-   */
-  /**
-   * @return NodeInterface
-   */
-  public function getData() {
-    return $this->eventTitle->get();
+  public function titleCase(&$variables):string {
+    $node = $variables['node'];
+//  dd($node);
+    $title = $node->get('title')->getValue()[0]['value'];
+//  dd($title);
+    if ($node->get('type')->getValue()[0]['target_id'] === 'event') {
+      $smallwordsarray = ['of','a','the','and','an','or','nor','but','is','if','then','else','when',
+        'at','from','by','on','off','for','in','out','over','to','into','with','so',
+        'yet','both', 'much','though', 'why', 'what', 'until', 'while', 'even', 'as'
+      ];
+      $words = explode(' ', $title);
+      foreach ($words as $key => $word) {
+        if (!$key or !in_array($word, $smallwordsarray))
+          $words[$key] = ucwords($word);
+      }
+      $newtitle = implode(' ', $words);
+    }
+//  dd($newtitle);
+    return $newtitle;
   }
 
 }
